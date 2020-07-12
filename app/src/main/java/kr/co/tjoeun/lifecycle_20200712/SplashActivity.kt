bbe5b93e7d74cf7026e8478430cf835b2f41974f
황선혜 <ghkdtjs6451@gmail.com>
2020-07-12 13:48:51 +0900

    로딩화면에서 2.5초 후에 메인화면으로 이동

--- app/src/main/java/kr/co/tjoeun/lifecycle_20200712/SplashActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200712/SplashActivity.kt
@@ -1,7 +1,10 @@
 package kr.co.tjoeun.lifecycle_20200712
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import java.security.DomainCombiner
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,18 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setupValues() {
+//        Handler를 변수를 만들고, 이 handler로 2.5초 후에 메인으로 이동.
+        val myHandler = Handler()
+
+        myHandler.postDelayed({
+
+//            일정시간이 지난 후에 실행할 내용
+            //        메인화면으로 이동
+            val myIntent = Intent(mContext, MainActivity::class.java)
+            startActivity(myIntent)
+
+        }, 2500)
+
 
     }
 }
