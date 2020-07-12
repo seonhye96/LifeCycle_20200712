d71c7a0945b88efefc76de0db28e17286f245b98
황선혜 <ghkdtjs6451@gmail.com>
2020-07-12 12:27:38 +0900

    다른 화면의 onCreate 확인

--- app/src/main/java/kr/co/tjoeun/lifecycle_20200712/OtherActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200712/OtherActivity.kt
@@ -2,10 +2,13 @@ package kr.co.tjoeun.lifecycle_20200712
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class OtherActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_other)
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_other)
+
+                Log.d("e다른화면", "onCreate 실행")
     }
 }
